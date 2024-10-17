5960abe6a0721a2b6d76251f501bd30ca432486e
rikoarik <rikoarik04@gmail.com>
2024-10-17 15:46:57 +0700

    message: add page leave

---
+++ app/src/main/java/com/hrmapps/data/model/Leave.kt
@@ -0,0 +1,8 @@
+package com.hrmapps.data.model
+
+data class Leave(
+    val tanggalCuti: String,
+    val tanggalMasuk: String,
+    val typeCuti: String,
+    val alasan: String
+)
--- app/src/main/java/com/hrmapps/ui/view/activity/LeaveActivity.kt
+++ app/src/main/java/com/hrmapps/ui/view/activity/LeaveActivity.kt
@@ -5,17 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hrmapps.R
+import com.hrmapps.data.model.Leave
+import com.hrmapps.databinding.ActivityLeaveBinding
+import com.hrmapps.ui.view.LeaveAdapter
 
 class LeaveActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLeaveBinding
+    private lateinit var leaveAdapter: LeaveAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_leave)
+        binding = ActivityLeaveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val leaveList = listOf(
+            Leave("01 Jan 2024", "05 Jan 2024", "Sakit", "Alasan cuti karena sakit demam."),
+            Leave("12 Feb 2024", "16 Feb 2024", "Libur", "Liburan keluarga.")
+        )
+
+        leaveAdapter = LeaveAdapter(leaveList)
+        binding.rvLeave.apply {
+            adapter = leaveAdapter
+            layoutManager = LinearLayoutManager(this@LeaveActivity)
         }
     }
 }

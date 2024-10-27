98ae2e9af41dfd40dc28fdd470802f84cee5b9e6
rikoarik <rikoarik04@gmail.com>
2024-10-27 14:35:49 +0700

    message: add SplashScreenActivity page

---
+++ app/src/main/java/com/hrmapps/ui/view/activity/SplashScreenActivity.kt
@@ -0,0 +1,44 @@
+package com.hrmapps.ui.view.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hrmapps.R
+import com.hrmapps.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("isLoggedIn", MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        Handler().postDelayed({
+            if (isLoggedIn) {
+                binding.progressBar.visibility = View.GONE
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }else{
+                binding.progressBar.visibility = View.GONE
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }, 3000)
+
+    }
+}

7a3476857fa992431981fce7192c107417a8e583
rikoarik <rikoarik04@gmail.com>
2024-11-25 19:04:17 +0700

    message: add check internet

--- app/src/main/java/com/hrmapps/ui/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/hrmapps/ui/view/activity/SplashScreenActivity.kt
@@ -1,13 +1,17 @@
 package com.hrmapps.ui.view.activity
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,7 +25,6 @@ class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
     private lateinit var sharedPreferences: SharedPreferences
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,7 +33,12 @@ class SplashScreenActivity : AppCompatActivity() {
         sharedPreferences = getSharedPreferences("isLoggedIn", MODE_PRIVATE)
         val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
         setupAnimation()
-        navigateAfterDelay(isLoggedIn)
+
+        if (isInternetAvailable()) {
+            navigateAfterDelay(isLoggedIn)
+        } else {
+            showNoInternetMessage()
+        }
     }
 
     private fun setupAnimation() {
@@ -49,5 +57,17 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(Intent(this, nextActivity))
             finish()
         }, 3000)
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+    }
+
+    private fun showNoInternetMessage() {
+        binding.lavLoading.visibility = View.GONE
+        Toast.makeText(this, "No internet connection. Please check your network.", Toast.LENGTH_LONG).show()
     }
 }
